734bc7d34228a46f36936643b7e3e9f5b649803a
Jay Two <jaytwo@jaytwo.kr>
2026-08-21 10:54:58 +0900

    print

---
+++ mino/core/include/mino/core/string/print.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>
+#include <string_view>
+#include <sstream>
+
+namespace mino::core::string::print {
+
+    // 1. 단일 인자를 출력 스트림으로 보내는 헬퍼
+    template <typename T>
+    void write_arg(std::ostream& os, const T& arg) {
+        os << arg;
+    }
+
+    // 2. 가변 인자를 순차적으로 포맷 스트링에 대입하는 헬퍼
+    template <typename... Args>
+    void format_to(std::ostream& os, std::string_view fmt, const Args&... args) {
+        size_t last_pos = 0;
+        auto replace_next = [&](const auto& arg) {
+            size_t pos = fmt.find("{}", last_pos);
+            if (pos != std::string_view::npos) {
+                os << fmt.substr(last_pos, pos - last_pos);
+                write_arg(os, arg);
+                last_pos = pos + 2; // "{}" 길이만큼 건너뜀
+            }
+            };
+
+        // C++17 폴드 표현식으로 각 인자 순차 처리
+        (replace_next(args), ...);
+
+        // 마지막 남은 포맷 문자열 출력
+        if (last_pos < fmt.size()) {
+            os << fmt.substr(last_pos);
+        }
+    }
+
+    // 3. std::print 와 std::println 구현
+    template <typename... Args>
+    void print(std::string_view fmt, const Args&... args) {
+        format_to(std::cout, fmt, args...);
+    }
+
+    template <typename... Args>
+    void println(std::string_view fmt, const Args&... args) {
+        format_to(std::cout, fmt, args...);
+        std::cout << '\n';
+    }
+
+} 
--- mino/core/include/mino/core/string/string.hpp
+++ mino/core/include/mino/core/string/string.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
-#include "mino/core/string/mutex_string.hpp" 
-#include "mino/core/string/string_basic.hpp" 
-#include "mino/core/string/to_console_encoding.hpp" 
-#include "mino/core/string/to_string.hpp" 
-#include "mino/core/string/u8string.hpp" 
 #include "mino/core/string/encoding_function.hpp"
+#include "mino/core/string/from_console_encoding.hpp"
+#include "mino/core/string/mutex_string.hpp"
+#include "mino/core/string/print.hpp"
+#include "mino/core/string/string_basic.hpp" 
 #include "mino/core/string/tokenizer.hpp"
+#include "mino/core/string/to_console_encoding.hpp" 
+#include "mino/core/string/to_string.hpp"
+#include "mino/core/string/u8.hpp"
+#include "mino/core/string/u8string.hpp" 
+
+
 
 
